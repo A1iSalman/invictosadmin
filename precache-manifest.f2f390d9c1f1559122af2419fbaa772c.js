eba43907fd8a8b299d036517cfe6ac6c8a1964d6
Ali <alisalmanh@yahoo.com>
2020-03-21 19:00:03 +0300

    Updates

---
+++ precache-manifest.f2f390d9c1f1559122af2419fbaa772c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac3a07fc77155771afad4848a992121",
+    "url": "/invictosadmin/index.html"
+  },
+  {
+    "revision": "6f014678b68a4102cad9",
+    "url": "/invictosadmin/static/css/main.f5a18bf1.chunk.css"
+  },
+  {
+    "revision": "b881be0e08eb97b76a52",
+    "url": "/invictosadmin/static/js/2.58fa2f6c.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/invictosadmin/static/js/2.58fa2f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f014678b68a4102cad9",
+    "url": "/invictosadmin/static/js/main.08f9d270.chunk.js"
+  },
+  {
+    "revision": "4f502ab7a95e260e7231",
+    "url": "/invictosadmin/static/js/runtime-main.0301fb09.js"
+  }
+]);
